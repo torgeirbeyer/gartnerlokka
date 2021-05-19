21575154aae262aa0240b7c162c39f3266895437
Torgeir Beyer <torgeir.beyer@netlife.com>
2021-05-19 23:09:44 +0200

    Add hidden

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Form() {
   return (
     <form className="my-4" name="tilbakemelding" method="post">
+      <input type="hidden" name="form-name" value="tilbakemelding" />
       <div className="">
         <label htmlFor="message">Tilbakemelding</label> <br />
         <textarea className="w-full p-2 my-2 border-2 rounded border-black" id="message" name="message" required rows="6"></textarea>
